83bcd1735e4db0c80f9744a1c7e606b4e4a62400
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-06 17:10:51 +0300

    1-batch_processing.py was created, it fetches rows in batches

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,31 @@
+# 1-batch_processing.py
+
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+            host='127.0.1.1',         
+            user='buna',     
+            password='tolosa',
+            database='ALX_prodev'     
+        )
+    
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM user_data")
+    
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                yield user
