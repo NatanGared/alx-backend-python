a05c9902ffecc532f6f6641d0d5b60140f8d12cf
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:32:10 +0300

    added the exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,15 +8,13 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
+        ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found in the nested map under 'a'")
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
-        ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found in the nested map under 'a'")
-    ])
     def test_access_nested_map_exception(self, nested_map, path, expected_exception, expected_message):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
