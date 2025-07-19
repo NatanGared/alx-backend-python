78baa4a1bc85e70f3138a982c37e5cd4c77e5030
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:48:01 +0300

    added the exception

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -4,6 +4,8 @@ def access_nested_map(nested_map, path):
             nested_map = nested_map[key]
         return nested_map
     except KeyError as e:
+        # Store the key that caused the KeyError
+        key = path[len(path) - 1]
         raise KeyError(f"Key '{key}' not found in the nested map") from e
     except (TypeError, IndexError):
         return "as expected"
