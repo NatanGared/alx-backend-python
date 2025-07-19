3bd7038ec42c32a438923daf4e70ea06feeb1227
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 13:45:18 +0300

    exception was added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
         @parameterized.expand([
                     ({}, ("a",), KeyError),
                     ({"a": 1}, ("a", "b"), KeyError)
-                    ])
+        ])
         def test_access_nested_map_exception(self, nested_map, path, expected_exception):
                     with self.assertRaises(expected_exception) as context:
                         utils.access_nested_map(nested_map, path)
