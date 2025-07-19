f287b2938d65dda55873666d3716870408f60309
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:28:52 +0300

    added the exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,8 +13,6 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-class TestAccessNestedMap(unittest.TestCase):
-
     @parameterized.expand([
         ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
         ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found in the nested map under 'a'")
