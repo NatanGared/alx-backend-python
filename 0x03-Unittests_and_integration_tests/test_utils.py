74800372158882a7a78c86307e7c697093509f92
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:23:58 +0300

    removed print and added the first line

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -10,9 +11,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        result = access_nested_map(nested_map, path)
-        print(f"Path: {path}, Expected: {expected}, Result: {result}")
-        self.assertEqual(result, expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
     unittest.main()
