6828d68a4cb246bc60c3d4eee06805ee47aa9524
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:15:20 +0300

    removed second code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,11 +4,7 @@ from parameterized import parameterized
 from test_utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
+    @parameterized.expand
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
