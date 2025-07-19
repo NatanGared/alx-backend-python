7a6ff68cd939ca3e1d4c878efe6c24a92319bbbb
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 00:25:13 +0300

    test_client.py file was created

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,17 +9,14 @@ class TestGithubOrgClient(TestCase):
 
     @parameterized.expand([
         ("google",),
-        ("abc",),
+        ("abc",)
     ])
     @patch('client.GithubOrgClient.get_json')
     def test_org(self, org_name, mock_get_json):
-        # Create an instance of GithubOrgClient
         github_client = GithubOrgClient()
 
-        # Call the org method
         github_client.org(org_name)
 
-        # Assert that get_json was called once with the expected URL
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
 
 if __name__ == '__main__':
