bc53e493c1c5c5bea141148553351cafec59a223
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 23:54:42 +0300

    added the exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
+
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -25,6 +27,23 @@ class TestAccessNestedMapExceptions(unittest.TestCase):
 
         expected = f"Key '{path[-1]}' not found in the nested map" if len(path) == 1 else f"Key '{path[-1]}' not found in the nested map under '{path[0]}'"
         self.assertEqual(str(context.exception), expected)
+
+class TestGetJson(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_json = Mock(return_value=test_payload)
+        mock_get.return_value.json = mock_json
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
