cdc8434e42088cbd5f55eab9a15dd11bd3aa305a
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-19 14:13:52 +0300

    multiple classes were merged in to one class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map  # Adjust the import based on your project structure
+from test_utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-
-        @parameterized.expand([
-                    ({"a": 1}, ("a",), 1),
-                    ({"a": {"b": 2}}, ("a",), {"b": 2}),
-                    ({"a": {"b": 2}}, ("a", "b"), 2),
-                    ])
-        def test_access_nested_map(self, nested_map, path, expected):
-                    self.assertEqual(access_nested_map(nested_map, path), expected)
-class TestAccessNestedMap(unittest.TestCase):
-
-        @parameterized.expand([
-                    ({}, ("a",), KeyError),
-                    ({"a": 1}, ("a", "b"), KeyError)
-        ])
-        def test_access_nested_map_exception(self, nested_map, path, expected_exception):
-                    with self.assertRaises(expected_exception) as context:
-                        utils.access_nested_map(nested_map, path)
-                    self.assertEqual(str(context.exception), "Key not found in nested map")
-
+    
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+        with self.assertRaises(expected_exception) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), "Key not found in nested map")
 
 if __name__ == '__main__':
     unittest.main()
