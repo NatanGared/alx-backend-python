649d0af521e8742b4edaeea042041fbaa7023534
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 10:54:09 +0300

    test_client.py file was created

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -8,3 +8,13 @@ def access_nested_map(nested_map, path):
         raise KeyError(f"Key '{key}' not found in the nested map") from e
     except (TypeError, IndexError):
         return "as expected"
+    
+def memoize(func):
+    cache = {}
+
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return wrapper
