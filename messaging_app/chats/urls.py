885a4fed9a1b9e8ee2744ab1c312f84f67930b54
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 15:41:48 +0300

    edited urls.py has been corrected to a working structure

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers 
+from rest_framework_nested import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-message')
+
+urlpatterns = router.urls + nested_router.urls
