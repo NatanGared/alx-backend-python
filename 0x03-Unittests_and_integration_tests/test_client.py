f2a5002dadb3c4a457f4a1c306b97ab396d58c31
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-20 11:10:46 +0300

    test_client.py file was created

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import unittest
-from unittest import TestCase
 from parameterized import parameterized
 from unittest.mock import patch
 from client import GithubOrgClient
@@ -12,12 +11,12 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc",)
     ])
     @patch('client.GithubOrgClient.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name, mock_org):
         github_client = GithubOrgClient()
 
         github_client.org(org_name)
 
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_org.assert_called_once_with(org_name)
 
 if __name__ == '__main__':
     unittest.main()
